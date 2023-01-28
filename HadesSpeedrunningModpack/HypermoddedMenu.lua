625fa7ba72780d5cb0c036932229d3ea65ee6f1b
Zyruvias <zerianledrias@gmail.com>
2023-01-28 18:03:14 -0500

    finish initial hypermodded UI beta

--- HadesSpeedrunningModpack/HypermoddedMenu.lua
+++ HadesSpeedrunningModpack/HypermoddedMenu.lua
@@ -501,14 +501,251 @@ function HSMConfigMenu.CreateBoonControlMenu ( screen )
   local itemSpacingX = 250
   local itemSpacingY = 65
 
+  -----------------
+  -- BoonControl.config.AllowHermesControl
+  -----------------
+  screen.Components["HermesControlEnabledTextBox"] = CreateScreenComponent({
+    Name = "BlankObstacle",
+    Scale = 1,
+    X = itemLocationX,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  CreateTextBox({
+    Id = screen.Components["HermesControlEnabledTextBox"].Id,
+    Text = "Enable control of Hermes:",
+    Color = Color.BoonPatchCommon,
+    FontSize = 16,
+    OffsetX = 0, OffsetY = 0,
+    Font = "AlegrayaSansSCRegular",
+    ShadowBlur = 0, ShadowColor = { 0, 0, 0, 1 }, ShadowOffset = { 0,  2 },
+    Justification = "Left"
+  })
+  
+  screen.Components["HermesControlControlEnabledCheckbox"] = CreateScreenComponent({
+    Name = "RadioButton",
+    Scale = 1,
+    X = itemLocationX + itemSpacingX * 3,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  screen.Components["HermesControlControlEnabledCheckbox"].Config = "BoonControl.config.AllowHermesControl"
+  screen.Components["HermesControlControlEnabledCheckbox"].OnPressedFunctionName = "HSMConfigMenu__ToggleGenericConfigCheckBox"
+  HSMConfigMenu__UpdateGenericConfigCheckbox(screen, screen.Components["HermesControlControlEnabledCheckbox"])
+  itemLocationY = itemLocationY + itemSpacingY
+  -----------------
+  -- BoonControl.config.AllowOlympianControl
+  -----------------
+  screen.Components["OlympianControlEnabledTextBox"] = CreateScreenComponent({
+    Name = "BlankObstacle",
+    Scale = 1,
+    X = itemLocationX,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  CreateTextBox({
+    Id = screen.Components["OlympianControlEnabledTextBox"].Id,
+    Text = "Enable control of other Olympians:",
+    Color = Color.BoonPatchCommon,
+    FontSize = 16,
+    OffsetX = 0, OffsetY = 0,
+    Font = "AlegrayaSansSCRegular",
+    ShadowBlur = 0, ShadowColor = { 0, 0, 0, 1 }, ShadowOffset = { 0,  2 },
+    Justification = "Left"
+  })
+  
+  screen.Components["OlympianControlEnabledCheckbox"] = CreateScreenComponent({
+    Name = "RadioButton",
+    Scale = 1,
+    X = itemLocationX + itemSpacingX * 3,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  screen.Components["OlympianControlEnabledCheckbox"].Config = "BoonControl.config.AllowOlympianControl"
+  screen.Components["OlympianControlEnabledCheckbox"].OnPressedFunctionName = "HSMConfigMenu__ToggleGenericConfigCheckBox"
+  HSMConfigMenu__UpdateGenericConfigCheckbox(screen, screen.Components["OlympianControlEnabledCheckbox"])
+  itemLocationY = itemLocationY + itemSpacingY
+
+  -----------------
+  -- BoonControl.config.AllowHammerControl
+  -----------------
+  
+  screen.Components["HammerControlTextBox"] = CreateScreenComponent({
+    Name = "BlankObstacle",
+    Scale = 1,
+    X = itemLocationX,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  CreateTextBox({
+    Id = screen.Components["HammerControlTextBox"].Id,
+    Text = "Select number of hammers to control:",
+    Color = Color.BoonPatchCommon,
+    FontSize = 16,
+    OffsetX = 0, OffsetY = 0,
+    Font = "AlegrayaSansSCRegular",
+    ShadowBlur = 0, ShadowColor = { 0, 0, 0, 1 }, ShadowOffset = { 0,  2 },
+    Justification = "Left"
+  })
+  local hammerOptions = {
+    Default = {
+      event = function (dropdown)
+        BoonControl.config.AllowedHammerControl = 0
+      end,
+      Text = "None"
+    },
+    [1] = {
+      event = function (dropdown)
+        BoonControl.config.AllowedHammerControl = 0
+      end,
+      Text = "None"
+    },
+    [2] = {
+      event = function (dropdown)
+        BoonControl.config.AllowedHammerControl = 1
+      end,
+      Text = "One Hammer"
+    },
+    [3] = {
+      event = function (dropdown)
+        BoonControl.config.AllowedHammerControl = 2
+      end,
+      Text = "Both Hammers"
+    },
+  }
+
+  ErumiUILib.Dropdown.CreateDropdown(screen, {
+    Name = "HammerControlDropdown",
+    Group = "Combat_Menu",  
+    Scale = {X = .25, Y = .5},
+    Padding = {X = 0, Y = 2},
+    X = itemLocationX + itemSpacingX * 3, Y = itemLocationY,
+    GeneralFontSize = 16,
+    Font = "AlegrayaSansSCRegular",
+    Items = hammerOptions,
+  })
+  itemLocationY = itemLocationY + itemSpacingY
   
 
+  -----------------
+  -- BoonControl.config.AllowRarityForce
+  -----------------
+  screen.Components["AllowRarityForceEnabledTextBox"] = CreateScreenComponent({
+    Name = "BlankObstacle",
+    Scale = 1,
+    X = itemLocationX,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  CreateTextBox({
+    Id = screen.Components["AllowRarityForceEnabledTextBox"].Id,
+    Text = "Enable for always-epic Hermes:",
+    Color = Color.BoonPatchCommon,
+    FontSize = 16,
+    OffsetX = 0, OffsetY = 0,
+    Font = "AlegrayaSansSCRegular",
+    ShadowBlur = 0, ShadowColor = { 0, 0, 0, 1 }, ShadowOffset = { 0,  2 },
+    Justification = "Left"
+  })
+  -- TODO: add note about all rarity force config possibilities but not leaderboard legal
+  
+  screen.Components["AllowRarityForceEnabledCheckbox"] = CreateScreenComponent({
+    Name = "RadioButton",
+    Scale = 1,
+    X = itemLocationX + itemSpacingX * 3,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  screen.Components["AllowRarityForceEnabledCheckbox"].Config = "BoonControl.config.AllowRarityForce"
+  screen.Components["AllowRarityForceEnabledCheckbox"].OnPressedFunctionName = "HSMConfigMenu__ToggleGenericConfigCheckBox"
+  HSMConfigMenu__UpdateGenericConfigCheckbox(screen, screen.Components["AllowRarityForceEnabledCheckbox"])
+  itemLocationY = itemLocationY + itemSpacingY
+
+  -----------------
+  -- BoonControl.config.FirstBoonAlwaysEpic/FirstBoonEpicOnPride
+  -----------------
+  
+  screen.Components["RarityControlTextBox"] = CreateScreenComponent({
+    Name = "BlankObstacle",
+    Scale = 1,
+    X = itemLocationX,
+    Y = itemLocationY,
+    Group = "Combat_Menu"
+  })
+  CreateTextBox({
+    Id = screen.Components["RarityControlTextBox"].Id,
+    Text = "Select first boon rarity settings:",
+    Color = Color.BoonPatchCommon,
+    FontSize = 16,
+    OffsetX = 0, OffsetY = 0,
+    Font = "AlegrayaSansSCRegular",
+    ShadowBlur = 0, ShadowColor = { 0, 0, 0, 1 }, ShadowOffset = { 0,  2 },
+    Justification = "Left"
+  })
+  local rarityOptions = {
+    Default = {
+      event = function (dropdown)
+        BoonControl.config.FirstBoonAlwaysEpic = false
+        BoonControl.config.FirstBoonEpicOnPride = false
+      end,
+      Text = "Vanilla"
+    },
+    [1] = {
+      event = function (dropdown)
+        BoonControl.config.FirstBoonAlwaysEpic = false
+        BoonControl.config.FirstBoonEpicOnPride = false
+      end,
+      Text = "Vanilla"
+    },
+    [2] = {
+      event = function (dropdown)
+        BoonControl.config.FirstBoonAlwaysEpic = false
+        BoonControl.config.FirstBoonEpicOnPride = true
+      end,
+      Text = "Epic on God's Pride"
+    },
+    [3] = {
+      event = function (dropdown)
+        BoonControl.config.FirstBoonAlwaysEpic = true
+        BoonControl.config.FirstBoonEpicOnPride = true
+      end,
+      Text = "Always Epic"
+    },
+  }
+
+  ErumiUILib.Dropdown.CreateDropdown(screen, {
+    Name = "RarityControlDropdown",
+    Group = "Combat_Menu",  
+    Scale = {X = .35, Y = .5},
+    Padding = {X = 0, Y = 2},
+    X = itemLocationX + itemSpacingX * 3, Y = itemLocationY,
+    GeneralFontSize = 16,
+    Font = "AlegrayaSansSCRegular",
+    Items = rarityOptions,
+  })
+  itemLocationY = itemLocationY + itemSpacingY
+
+end
+
+function UpdateDropdown( screen, dropdown, options, newSetValue )
+  if dropdown == nil then
+    return
+  end
+  -- copied from NewEntry function, using the Dropdown api led to weird crashes
+  dropdown.dropDownPressedArgs.Items = options
+  if dropdown.isExpanded then
+      ErumiUILib.Dropdown.Collapse(screen, dropdown)
+      ErumiUILib.Dropdown.Expand(screen, dropdown)
+  end
+  if newSetValue ~= nil then
+    ErumiUILib.Dropdown.SetValue(dropdown, newSetValue)
+  end
 end
 
 function HSMConfigMenu.CreateHermesMenu ( screen )
   local rowStartX = 250
   local itemLocationX = rowStartX
-  local itemLocationY = 500
+  local itemLocationY = 250
   local itemSpacingX = 250
   local itemSpacingY = 65
 
@@ -537,6 +774,7 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
     })
     return
   end
+  itemLocationY = 500
   --[[
     1. Create Weapon List Dropdown
     2. Create Aspect List Dropdown
@@ -553,9 +791,12 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
     local aspectIcon = TraitData[aspectID].Icon .. "_Large"
     SetAnimation({ DestinationId = screen.Components["AspectIcon"].Id, Name = aspectIcon})
   end
+  -- TODO: fix Default Weapon list not updating icon
+
+  local updateHermesDropdown = nil
 
   -- 4. On Select Function for Weapon List
-  local updateAspectListDropdown = function (aspects)
+  local onWeaponListDropdownChange = function (aspects)
     local hermesOptionsByAspect = {["Default"] = {
       event = function (dropdown)
         aspectToDisplay = aspects[1]
@@ -568,40 +809,20 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
       table.insert(hermesOptionsByAspect, {
         event = function (dropdown)
           changeDisplayAspect(aspectID)
+          updateHermesDropdown(RCLib.CodeToName.Aspects[aspectID])
         end,
         Text = aspectID
-        -- Text = RCLib.CodeToName.Aspects[aspectID]
       })
     end
-
-    if aspectDropdown ~= nil then
-      -- copied from NewEntry function, using the Dropdown api led to weird crashes
-      aspectDropdown.dropDownPressedArgs.Items = hermesOptionsByAspect
-      if aspectDropdown.isExpanded then
-          ErumiUILib.Dropdown.Collapse(screen, aspectDropdown)
-          ErumiUILib.Dropdown.Expand(screen, aspectDropdown)
-      end
-      -- TODO: update the current display text/default text? idk man
-      changeDisplayAspect(aspects[1])
-      ErumiUILib.Dropdown.SetValue(aspectDropdown, 1)
-
-    else -- should probably not get here but ya know it's for the best
-      ErumiUILib.Dropdown.CreateDropdown(screen, {
-        Name = "HermesAspectDropdown",
-        Group = "Combat_Menu",  
-        Scale = {X = .25, Y = .5},
-        Padding = {X = 0, Y = 2},
-        X = itemLocationX + itemSpacingX, Y = itemLocationY,
-        GeneralFontSize = 16,
-        Font = "AlegrayaSansSCRegular",
-        Items = hermesOptionsByAspect,
-      })
-    end
+    UpdateDropdown(screen, aspectDropdown, hermesOptionsByAspect, 1)
+    -- TODO: update the current display text/default text? idk man
+    changeDisplayAspect(aspects[1])
+    updateHermesDropdown(RCLib.CodeToName.Aspects[aspects[1]])
   end
 
   local weaponOptions = {["Default"] = {
     -- event = function (dropdown)
-    --   updateAspectListDropdown("SwordWeapon")
+    --   onWeaponListDropdownChange("SwordWeapon")
     -- end,
     Text = "SwordWeapon"
   }}
@@ -611,11 +832,13 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
     local weaponData = RunStartControl.WeaponAspectData[i]
     table.insert(weaponOptions, {
       event = function (dropdown)
-        updateAspectListDropdown(weaponData.Aspects)
+        onWeaponListDropdownChange(weaponData.Aspects)
       end,
       Text = weaponData.Name
     })
   end
+
+  local hermesSettingsByAspectDropdown = nil
 
   -- weapon dropdown
   ErumiUILib.Dropdown.CreateDropdown(screen, {
@@ -629,10 +852,36 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
     Items = weaponOptions,
   })
 
-  local changeDisplayAspect = function(aspectID)
-    -- set icon
-    local aspectIcon = TraitData[aspectID].Icon .. "_Large"
-    SetAnimation({ DestinationId = screen.Components["AspectIcon"].Id, Name = aspectIcon})
+  updateHermesDropdown = function(aspect)
+    DebugPrint{
+      Text = "Updating hermes options for " .. aspect ..
+      " with a default of " .. BoonControl.config.AspectSettings[aspect].BoonSetting
+    }
+    local hermesBoonOptions = {["Default"] = {
+      Text = BoonControl.config.AspectSettings[aspect].BoonSetting
+    }}
+    
+    for k, v in pairs(BoonControl.BoonPresets) do
+      table.insert(hermesBoonOptions, {
+        event = function (dropdown)
+          DebugPrint { Text = "Updating " .. aspect .. " to " .. k}
+          BoonControl.config.AspectSettings[aspect].BoonSetting = k
+        end,
+        Text = k
+      })
+    end
+    UpdateDropdown(
+      screen,
+      hermesSettingsByAspectDropdown,
+      hermesBoonOptions,
+      BoonControl.config.AspectSettings[aspect].BoonSetting
+    )
+    
+  end
+
+  local onAspectChange = function(aspectID)
+
+    updateHermesDropdown(RCLib.CodeToName.Aspects[aspectID])
   end
 
   -- Create Hermes Control Menu
@@ -647,7 +896,7 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
       local aspectID = weaponData.Aspects[j]
       table.insert(hermesOptionsByAspect, {
         event = function (dropdown)
-          changeDisplayAspect(aspectID)
+          onAspectChange(aspectID)
         end,
         Text = aspectID
       })
@@ -677,23 +926,9 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
   SetAnimation({ DestinationId = screen.Components["AspectIcon"].Id, Name = aspectIcon})
   
 
-  -- actual hermes boons options
   itemLocationX = itemLocationX + itemSpacingX * 4
-  local hermesBoonOptions = {["Default"] = {
-    Text = BoonControl.config.AspectSettings[currentAspect].BoonSetting
-  }}
-  
-  for k, v in pairs(BoonControl.BoonPresets) do
-      table.insert(hermesBoonOptions, {
-        event = function (dropdown)
-          DebugPrint({ Text = (aspectDropdown.currentItem.Text or currentAspect) .. " " .. k})
-          BoonControl.config.AspectSettings[aspectDropdown.currentItem.Text or currentAspect].BoonSetting = k
-        end,
-        Text = k
-      })
-  end
-
-  ErumiUILib.Dropdown.CreateDropdown(screen, {
+  -- actual hermes boons options
+  hermesSettingsByAspectDropdown = ErumiUILib.Dropdown.CreateDropdown(screen, {
     Name = "HermesBoonDropdown",
     Group = "Combat_Menu",  
     Scale = {X = .3, Y = .5},
@@ -701,9 +936,13 @@ function HSMConfigMenu.CreateHermesMenu ( screen )
     X = itemLocationX, Y = itemLocationY,
     GeneralFontSize = 16,
     Font = "AlegrayaSansSCRegular",
-    Items = hermesBoonOptions,
+    -- placeholder, this gets updated very quickly anyway
+    Items = {["Default"] = {
+      Text = BoonControl.config.AspectSettings[currentAspect].BoonSetting
+    }},
   })
-
+  
+  updateHermesDropdown(currentAspect)
 end
 
 ModUtil.LoadOnce(function()
